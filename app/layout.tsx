60be56b0cc5c97e6d9a69e72506fc5d0ec4f08aa
Maxim-Coder-arch <maximnovikov2206@gmail.com>
2026-04-04 18:54:10 +0300

    add metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./repetitiveComponents/footer/footer";
 import MobileMenu from "./repetitiveComponents/mobileMenu";
 import { metaData } from "../metadata/metadata";
 import { Tracker } from "./components/Tracker";
+import Script from "next/script";
 
 export const metadata: Metadata = metaData;
 
@@ -19,6 +20,24 @@ export default function RootLayout({
         <Menu />
         <MobileMenu />
         {children}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(108393702, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+            });
+          `}
+        </Script>
+
         <Footer />
         <Tracker />
       </body>
